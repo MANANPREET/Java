b80d82e213d45e7c134fa8ae40532dbe571feb76
MANANPREET <mananpreetsingh.win@gmail.com>
2017-04-16 11:48:22 +0530

    Changes to beans.xml

--- src/main/java/com/manan/spring/test/Person.java
+++ src/main/java/com/manan/spring/test/Person.java
@@ -1,4 +1,5 @@
 package com.manan.spring.test;
+
 /**
  * 
  * @author Manan
@@ -7,20 +8,20 @@ package com.manan.spring.test;
 public class Person {
 	private int id;
 	private String name;
-	
+
 	public Person() {
-		
+
 	}
+
 	public Person(int id, String name) {
 		this.id = id;
 		this.name = name;
 	}
 
-
 	public void speak() {
 		System.out.println("Hello I am a person");
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Person [id=" + id + ", name=" + name + "]";
