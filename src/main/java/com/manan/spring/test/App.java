0b0267c75696dba98f6ff70a392244dbdef6c272
MANANPREET <mananpreetsingh.win@gmail.com>
2017-04-16 10:47:47 +0530

    Initial commit : Basic spring bean project

---
+++ src/main/java/com/manan/spring/test/App.java
@@ -0,0 +1,19 @@
+package com.manan.spring.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+/**
+ * 
+ * @author Manan
+ *
+ */
+public class App {
+
+	public static void main(String[] args) {
+	ApplicationContext context = new ClassPathXmlApplicationContext("com/manan/spring/test/beans/beans.xml");
+	Person person = (Person) context.getBean("person");
+	person.speak();
+	((ClassPathXmlApplicationContext)context).close();
+	}
+}
---
+++ src/main/java/com/manan/spring/test/Person.java
@@ -0,0 +1,11 @@
+package com.manan.spring.test;
+/**
+ * 
+ * @author Manan
+ *
+ */
+public class Person {
+	public void speak() {
+		System.out.println("Hello I am a person");
+	}
+}
