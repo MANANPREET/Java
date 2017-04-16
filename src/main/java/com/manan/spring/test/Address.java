94ef3fe5b5c0aa83f79f3ff6d4116e5ed646a1c5
MANANPREET <mananpreetsingh.win@gmail.com>
2017-04-16 12:13:06 +0530

    Added Address.java

---
+++ src/main/java/com/manan/spring/test/Address.java
@@ -0,0 +1,24 @@
+package com.manan.spring.test;
+
+/**
+ * 
+ * @author Manan
+ *
+ */
+public class Address {
+
+	private String street;
+	private String postcode;
+	
+	public Address(String street, String postcode) {
+		this.street = street;
+		this.postcode = postcode;
+	}
+	
+	@Override
+	public String toString() {
+		return "Address [street=" + street + ", postcode=" + postcode + "]";
+	}
+	
+	
+}
