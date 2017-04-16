922646d36fd0f26713982e12c5f3ba658842417c
MANANPREET <mananpreetsingh.win@gmail.com>
2017-04-16 12:12:04 +0530

    Added new class Address.java and made changes to beans.xml to add new bean and also made changes to App.java

--- src/main/java/com/manan/spring/test/App.java
+++ src/main/java/com/manan/spring/test/App.java
@@ -13,8 +13,10 @@ public class App {
 	public static void main(String[] args) {
 	ApplicationContext context = new ClassPathXmlApplicationContext("com/manan/spring/test/beans/beans.xml");
 	Person person = (Person) context.getBean("person");
+	Address address = (Address) context.getBean("address");
 	person.speak();
 	System.out.println(person);
+	System.out.println(address);
 	((ClassPathXmlApplicationContext)context).close();
 	}
 }
