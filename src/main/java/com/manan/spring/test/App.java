dd8266514653f8848f0f5a4248918cc919e6e1f5
MANANPREET <mananpreetsingh.win@gmail.com>
2017-04-16 12:38:52 +0530

    Person bean Scope changed to "Prototype"

--- src/main/java/com/manan/spring/test/App.java
+++ src/main/java/com/manan/spring/test/App.java
@@ -13,9 +13,11 @@ public class App {
 	public static void main(String[] args) {
 	ApplicationContext context = new ClassPathXmlApplicationContext("com/manan/spring/test/beans/beans.xml");
 	Person person = (Person) context.getBean("person");
+	person.setTaxId(6666);
+	Person person2 = (Person) context.getBean("person");
 	Address address = (Address) context.getBean("address");
 	person.speak();
-	System.out.println(person);
+	System.out.println(person2);
 	System.out.println(address);
 	((ClassPathXmlApplicationContext)context).close();
 	}
